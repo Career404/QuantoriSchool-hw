bb124564c47f74d43e119d5f7e330017d6fea0f0
Career404 <105638831+Career404@users.noreply.github.com>
2023-03-12 13:53:28 +0400

    Revert "Merge branch '1phoneBlur' into 1htmlStatic"
    This reverts commit de7ebc86d2b1010f78146a082834dec47ccc6d4c.

--- src/focus.js
+++
@@ -1,5 +0,0 @@
-const focusDiv = document.getElementById('focus')
-document.addEventListener('mousemove', (e) => {
-	focusDiv.style.top = e.clientY + 'px'
-	focusDiv.style.left = e.clientX + 'px'
-})
