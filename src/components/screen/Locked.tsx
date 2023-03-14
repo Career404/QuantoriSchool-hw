d2c2a67afe3f785d7740176fd54cee9272a923fc
Career404 <105638831+Career404@users.noreply.github.com>
2023-03-14 20:11:46 +0400

    feat: add swipeToUnlock text

--- src/components/screen/Locked.tsx
+++ src/components/screen/Locked.tsx
@@ -1,10 +1,46 @@
 import React, { PropsWithChildren } from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import lockBG from '../../assets/diagmonds.png'
+import lockIcon from '../../assets/arrow-right.png'
 
 const Full = styled.div`
 	height: 100%;
 	width: 100%;
+`
+const LockDiv = styled.div`
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	height: 30%;
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: center;
+	align-items: center;
+`
+const swipeTextAnim = keyframes`
+	0% {
+		background-position: 0;
+	}
+	100% {
+		background-position: 100px;
+	}
+`
+const LockText = styled.div`
+	user-select: none;
+	color: #ffffff;
+	background: linear-gradient(to right, #4d4d4d 0, white 10%, #4d4d4d 20%);
+	background-position: 0;
+	-webkit-background-clip: text;
+	background-clip: text;
+	-webkit-text-fill-color: transparent;
+	animation: ${swipeTextAnim} 6s infinite linear;
+`
+const LockIcon = styled.div`
+	height: 1.5rem;
+	width: 1.5rem;
+	background-image: url(${lockIcon});
+	background-size: contain;
 `
 interface LockProps extends PropsWithChildren {
 	isLocked: boolean
@@ -23,6 +59,10 @@ export default function LockedScreen({
 	return (
 		<Full style={{ backgroundImage: bg, backgroundColor: bgColor }}>
 			{children}
+			<LockDiv>
+				<LockIcon />
+				<LockText>Swipe to unlock</LockText>
+			</LockDiv>
 		</Full>
 	)
 }
