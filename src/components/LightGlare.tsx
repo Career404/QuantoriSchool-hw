a16576889f45edc3ba306851ad836366fbcfb31f
Career404 <105638831+Career404@users.noreply.github.com>
2023-03-14 19:05:13 +0400

    fix: improve visuals on large screens

--- src/components/LightGlare.tsx
+++ src/components/LightGlare.tsx
@@ -5,18 +5,18 @@ const FullScreenContainer = styled.div`
 	pointer-events: none;
 	position: absolute;
 	top: 0;
+	bottom: 0;
 	left: 0;
+	right: 0;
 	overflow: hidden;
-	width: 100%;
-	height: 100%;
 `
 const Light = styled.div`
 	display: block;
-	width: 90vw;
-	height: 150vh;
+	width: 100vw;
+	height: 200vh;
 	transform: rotate(25deg) translate(-80%, -20%);
 	background: rgba(255, 255, 255, 0.18);
-	filter: blur(15px);
+	filter: blur(25px);
 `
 
 export default function LightGlare() {
