a6c3ceb14a99d4637825574ff5832527fe7b70ce
Career404 <105638831+Career404@users.noreply.github.com>
2023-03-20 17:48:05 +0400

    feat: multi-page

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import './style.css'
+
+function Main() {
+	return (
+		<main>
+			<h1>Quantori-homework</h1>
+			<h2>Dmitry Zhukov</h2>
+			<ol>
+				<li>
+					<a href="src\pages\1phone\1phone.html">Smartphone</a>
+				</li>
+			</ol>
+		</main>
+	)
+}
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<Main />
 	</React.StrictMode>
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
@@ -5,4 +6,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
 	plugins: [react()],
 	base: '/QuantoriSchool-hw/',
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				phone: resolve(__dirname, 'src/pages/1phone/1phone.html'),
+			},
+		},
+	},
 })
