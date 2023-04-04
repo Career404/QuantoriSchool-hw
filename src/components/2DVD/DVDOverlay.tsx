1789116fd58e35ec1b39ad5fda2f50bbd28c0da0
Career404 <105638831+Career404@users.noreply.github.com>
2023-04-04 17:40:51 +0400

    feat: create DVD component

--- src/components/2DVD/DVDOverlay.tsx
+++ src/components/2DVD/DVDOverlay.tsx
@@ -1,2 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+const Fullscreen = styled.div`
+	position: fixed;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	background-color: black;
+`;
+
+export default function DVDOverlay() {
+	return <Fullscreen></Fullscreen>;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './style.css'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+
+import DVDOverlay from './components/2DVD/DVDOverlay';
 
 function Main() {
+	const [showDVD, setShowDVD] = useState(false);
 	return (
 		<main>
 			<h1>Quantori-homework</h1>
@@ -12,15 +15,18 @@ function Main() {
 					<a href="src\pages\1phone\1phone.html">Smartphone</a>
 				</li>
 				<li>
-					<a href="src\pages\2DVD\2DVD.html">DVD Screensaver</a>
+					<a href="#" onClick={() => setShowDVD(!showDVD)}>
+						DVD Screensaver
+					</a>
 				</li>
 			</ol>
+			{showDVD && <DVDOverlay />}
 		</main>
-	)
+	);
 }
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
 	<React.StrictMode>
 		<Main />
 	</React.StrictMode>
-)
+);
