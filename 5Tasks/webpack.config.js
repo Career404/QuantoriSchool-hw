acd3309ee761258364c0caa0e58d42295b8ababd
Career404 <105638831+Career404@users.noreply.github.com>
2023-04-22 12:39:17 +0400

    feat: add and configure html-webpack-plugin

--- 5Tasks/webpack.config.js
+++ 5Tasks/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
@@ -7,4 +8,11 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'To Do app',
+			scriptLoading: 'module',
+			favicon: path.resolve(__dirname, 'src/assets/favicon.png'),
+		}),
+	],
 };
