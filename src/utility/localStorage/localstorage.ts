e108bf291c4bf7bb7234e71708b6f293617aafdf
Career404 <105638831+Career404@users.noreply.github.com>
2023-05-02 12:21:56 +0400

    feat: useLocalStorage hook

--- src/utility/localStorage/localstorage.ts
+++ src/utility/localStorage/localstorage.ts
@@ -1,5 +1,27 @@
-export const setStorage = (name: string, state: any) =>
-	localStorage.setItem(name, JSON.stringify(state));
+import { useState } from 'react';
 
-export const getStorage = (name: string) =>
-	JSON.parse(localStorage.getItem(name)!);
+export default function useLocalStorage<T>(
+	key: string,
+	initialValue: T
+): [T, (value: T) => void] {
+	const [storedValue, setStoredValue] = useState(() => {
+		try {
+			const item = localStorage.getItem(key);
+			return item ? JSON.parse(item) : initialValue;
+		} catch (error) {
+			console.error(error);
+			return initialValue;
+		}
+	});
+
+	const setValue = (value: T) => {
+		try {
+			setStoredValue(value);
+			localStorage.setItem(key, JSON.stringify(value));
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	return [storedValue, setValue];
+}
